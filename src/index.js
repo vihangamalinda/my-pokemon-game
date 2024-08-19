45903f698af37c0b3e1dfe1d42799e17d15c0de8
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2024-08-19 14:00:06 -0800

    feat:Adding movements for the player.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import Sprite from "./sprite.js";
+
 const canvas = document.querySelector("canvas");
 const context = canvas.getContext("2d");
 const MAP_SRC = "../assets/Pellet_Town.png";
@@ -18,16 +20,95 @@ const playerImage = new Image();
 playerImage.src = PLAYER_MAIN_SRC + "/playerDown.png";
 console.log(playerImage);
 
-imageMap.addEventListener("load", () => {
-  context.drawImage(imageMap, -785, -600);
-  // context.drawImage(player,450,350);
-  console.log(imageMap);
-});
+// imageMap.addEventListener("load", () => {
+//   context.drawImage(imageMap, -785, -660);
+//   // context.drawImage(player,450,350);
+//   console.log(imageMap);
+// });
 
-const playerStart_X = (CONTAINER_WITDTH * 0.5)- (playerImage.width*0.7) * 0.5;
+const playerStart_X = CONTAINER_WITDTH * 0.5 - playerImage.width * 0.7 * 0.5;
 const playerStart_Y = (CONTAINER_HEIGHT - playerImage.height) * 0.5;
 
-playerImage.addEventListener("load", () => {
+// playerImage.addEventListener("load", () => {
+
+// });
+
+const keys = {
+  w: {
+    pressed: false,
+  },
+  d: {
+    pressed: false,
+  },
+  s: {
+    pressed: false,
+  },
+  a: {
+    pressed: false,
+  },
+};
+
+window.addEventListener("keydown", (event) => {
+  switch (event.key) {
+    case "w":
+      // console.log(event.key);
+      keys.w.pressed = true;
+      break;
+    case "d":
+      // console.log(event.key);
+      keys.d.pressed = true;
+      break;
+    case "s":
+      // console.log(event.key);
+      keys.s.pressed = true;
+      break;
+    case "a":
+      // console.log(event.key);
+      keys.a.pressed = true;
+      // console.log(keys.a.pressed);
+      break;
+  }
+  // console.log(keys);
+});
+
+window.addEventListener("keyup", (event) => {
+  switch (event.key) {
+    case "w":
+      // console.log(event.key);
+      keys.w.pressed = false;
+      break;
+    case "d":
+      // console.log(event.key);
+      keys.d.pressed = false;
+      break;
+    case "s":
+      // console.log(event.key);
+      keys.s.pressed = false;
+      break;
+    case "a":
+      // console.log(event.key);
+      keys.a.pressed = false;
+      // console.log(keys.a.pressed);
+      break;
+  }
+  // console.log(keys);
+});
+
+// TODO: need to identify if its ok to pass context to sprite
+//
+const background = new Sprite(
+  context,
+  { x: -785, y: -650 },
+  undefined,
+  imageMap
+);
+// console.log(context);
+let y = 0;
+(function animate() {
+  window.requestAnimationFrame(animate);
+  // y++;
+  // context.drawImage(imageMap, -785, -650);
+  background.draw();
   context.drawImage(
     playerImage,
     0,
@@ -35,10 +116,19 @@ playerImage.addEventListener("load", () => {
     playerImage.width * 0.25,
     playerImage.height,
     playerStart_X,
-    playerStart_Y,
+    playerStart_Y +   0.3,
     playerImage.width * 0.25,
     playerImage.height
   );
-});
-
-console.log(context);
+  // console.log(y)
+  const movement = 3;
+  if (keys.w.pressed) {
+    background.moveUp(movement);
+  } else if (keys.d.pressed) {
+    background.moveRight(movement);
+  } else if (keys.s.pressed) {
+    background.moveDown(movement);
+  } else if (keys.a.pressed) {
+    background.moveLeft(movement);
+  }
+})();
---
+++ src/sprite.js
@@ -0,0 +1,24 @@
+export default class Sprite {
+  constructor(canvasContext, position, velocity, image) {
+    this.canvasContext = canvasContext;
+    this.position = position;
+    this.image = image;
+  }
+
+  draw() {
+    this.canvasContext.drawImage(this.image, this.position.x, this.position.y);
+  }
+
+  moveUp(value) {
+    this.position.y+=value;
+  }
+  moveDown(value) {
+    this.position.y-=value;
+  }
+  moveLeft(value) {
+    this.position.x+=value;
+  }
+  moveRight(value) {
+    this.position.x-=value;
+  }
+}
