9802079f0109a0acb1abb451c6c8a5dc4bb9815e
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2024-08-31 16:54:39 -0800

    refactor:Encapsulating the feild properies with private(#).

--- src/sprite.js
+++ src/sprite.js
@@ -1,24 +1,26 @@
 export default class Sprite {
+  #position;
+  #image;
   constructor(canvasContext, position, velocity, image) {
     this.canvasContext = canvasContext;
-    this.position = position;
-    this.image = image;
+    this.#position = position;
+    this.#image = image;
   }
 
   draw() {
-    this.canvasContext.drawImage(this.image, this.position.x, this.position.y);
+    this.canvasContext.drawImage(this.#image, this.#position.x, this.#position.y);
   }
 
   moveUp(value) {
-    this.position.y+=value;
+    this.#position.y+=value;
   }
   moveDown(value) {
-    this.position.y-=value;
+    this.#position.y-=value;
   }
   moveLeft(value) {
-    this.position.x+=value;
+    this.#position.x+=value;
   }
   moveRight(value) {
-    this.position.x-=value;
+    this.#position.x-=value;
   }
 }
