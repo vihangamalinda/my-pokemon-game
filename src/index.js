3cace3c939305245e13f0cc132e39793115df339
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2024-09-01 11:42:22 -0800

    feat:Console.loging if collsion happens between single obstcle and a player.

--- src/index.js
+++ src/index.js
@@ -9,10 +9,10 @@ const PLAYER_MAIN_SRC = "../assets/player_character";
 const CONTAINER_WITDTH = 1024;
 const CONTAINER_HEIGHT = 576;
 
-const offSet ={
-  x:-785,
-  y:-650
-}
+const offSet = {
+  x: -785,
+  y: -650,
+};
 
 canvas.width = CONTAINER_WITDTH;
 canvas.height = CONTAINER_HEIGHT;
@@ -53,12 +53,28 @@ class Boundary {
   constructor({ position }) {
     this.position = position;
     this.width = Boundary.resizedPixel;
-    this.height =Boundary.resizedPixel;
+    this.height = Boundary.resizedPixel;
   }
 
   draw() {
     context.fillStyle = "red";
     context.fillRect(this.position.x, this.position.y, this.width, this.height);
+  }
+
+  moveUp(val) {
+    this.position.y += val;
+  }
+
+  moveDown(val) {
+    this.position.y -= val;
+  }
+
+  moveRight(val) {
+    this.position.x -= val;
+  }
+
+  moveLeft(val) {
+    this.position.x += val;
   }
 
   static isBoundary(val) {
@@ -73,8 +89,8 @@ collisionMap.forEach((row, mapRowNum) =>
     if (Boundary.isBoundary(item)) {
       const boundry = {
         position: {
-          x: mapColNum * Boundary.resizedPixel +offSet.x,
-          y: mapRowNum * Boundary.resizedPixel+offSet.y,
+          x: mapColNum * Boundary.resizedPixel + offSet.x,
+          y: mapRowNum * Boundary.resizedPixel + offSet.y,
         },
       };
       boundaries.push(new Boundary(boundry));
@@ -86,18 +102,11 @@ console.dir(boundaries);
 
 // ********************
 
-// imageMap.addEventListener("load", () => {
-//   context.drawImage(imageMap, -785, -660);
-//   // context.drawImage(player,450,350);
-//   console.log(imageMap);
-// });
+const playerImageWidth = 192;
+const playerImageHeight = 68;
 
-const playerStart_X = CONTAINER_WITDTH * 0.5 - playerImage.width * 0.7 * 0.5;
-const playerStart_Y = (CONTAINER_HEIGHT - playerImage.height) * 0.5;
-
-// playerImage.addEventListener("load", () => {
-
-// });
+const playerStart_X = CONTAINER_WITDTH * 0.5 - playerImageWidth * 0.7 * 0.5;
+const playerStart_Y = (CONTAINER_HEIGHT - playerImageHeight) * 0.5;
 
 const keys = {
   w: {
@@ -120,28 +129,22 @@ const isLastKey = (keyVal) => {
 window.addEventListener("keydown", (event) => {
   switch (event.key) {
     case "w":
-      // console.log(event.key);
       keys.w.pressed = true;
       lastKey = "w";
       break;
     case "d":
-      // console.log(event.key);
       keys.d.pressed = true;
       lastKey = "d";
       break;
     case "s":
-      // console.log(event.key);
       keys.s.pressed = true;
       lastKey = "s";
       break;
     case "a":
-      // console.log(event.key);
       keys.a.pressed = true;
-      // console.log(keys.a.pressed);
       lastKey = "a";
       break;
   }
-  // console.log(keys);
 });
 
 window.addEventListener("keyup", (event) => {
@@ -171,39 +174,71 @@ window.addEventListener("keyup", (event) => {
 //
 const background = new Sprite(
   context,
-  { x: offSet.x, y: offSet.y},
+  { x: offSet.x, y: offSet.y },
   undefined,
   imageMap
 );
-// console.log(context);
 
-let y = 0;
+const testBoundary = new Boundary({
+  position: {
+    x: 400,
+    y: 400,
+  },
+});
+
+const movables = [background, testBoundary];
+
+const player = new Sprite(
+  context,
+  {
+    x: playerStart_X,
+    y: playerStart_Y,
+  },
+  0,
+  playerImage,
+  { max: 4 }
+);
+
 (function animate() {
   window.requestAnimationFrame(animate);
-  // y++;
-  // context.drawImage(imageMap, -785, -650);
   background.draw();
-  boundaries.forEach(boundary=>boundary.draw())
-  context.drawImage(
-    playerImage,
-    0,
-    0,
-    playerImage.width * 0.25,
-    playerImage.height,
-    playerStart_X,
-    playerStart_Y + 0.3,
-    playerImage.width * 0.25,
-    playerImage.height
-  );
-  // console.log(y)
+  // boundaries.forEach(boundary=>boundary.draw());
+  testBoundary.draw();
+  player.draw();
+
+  collisionByPlayerMovement({ rectangle1: player, rectangle2: testBoundary });
+
   const movement = 3;
   if (keys.w.pressed && isLastKey("w")) {
-    background.moveUp(movement);
+    movables.forEach((movableUnit) => movableUnit.moveUp(movement));
   } else if (keys.d.pressed && isLastKey("d")) {
-    background.moveRight(movement);
+    movables.forEach((movableUnit) => movableUnit.moveRight(movement));
   } else if (keys.s.pressed && isLastKey("s")) {
-    background.moveDown(movement);
+    movables.forEach((movableUnit) => movableUnit.moveDown(movement));
   } else if (keys.a.pressed && isLastKey("a")) {
-    background.moveLeft(movement);
+    movables.forEach((movableUnit) => movableUnit.moveLeft(movement));
   }
 })();
+
+// Collision logic
+function collisionByPlayerMovement({ rectangle1, rectangle2 }) {
+  const isCollidedByMovingRight =
+    rectangle1.getPosition().x + rectangle1.singleFrameWidth >=
+    rectangle2.position.x;
+  const isCollidedByMovingDown =
+    rectangle1.getPosition().y + rectangle1.singleFrameHeight >=
+    rectangle2.position.y;
+  const isCollidedByMovingLeft =
+    rectangle1.getPosition().x <=
+    rectangle2.position.x + rectangle1.singleFrameWidth;
+  const isCollidedByMovingUp =
+    rectangle1.getPosition().y <=
+    rectangle2.position.y + rectangle1.singleFrameWidth;
+
+  const isCollidedByXMovement =
+    isCollidedByMovingRight && isCollidedByMovingLeft;
+  const isCollidedByYMovement = isCollidedByMovingDown && isCollidedByMovingUp;
+  if (isCollidedByXMovement && isCollidedByYMovement) {
+    console.log("colliding");
+  }
+}
--- src/sprite.js
+++ src/sprite.js
@@ -1,26 +1,49 @@
 export default class Sprite {
   #position;
   #image;
-  constructor(canvasContext, position, velocity, image) {
+  #frames;
+
+  constructor(canvasContext, position, velocity, image, frames = { max: 1 }) {
     this.canvasContext = canvasContext;
     this.#position = position;
     this.#image = image;
+    this.#frames = frames;
+    addEventListener("load", () => this.#setSingelFrameProperites());
+  }
+
+  #setSingelFrameProperites() {
+    this.singleFrameWidth = this.#image.width / this.#frames.max;
+    this.singleFrameHeight = this.#image.height;
   }
 
   draw() {
-    this.canvasContext.drawImage(this.#image, this.#position.x, this.#position.y);
+    this.canvasContext.drawImage(
+      this.#image,
+      0,
+      0,
+      this.#image.width / this.#frames.max,
+      this.#image.height,
+      this.#position.x,
+      this.#position.y,
+      this.#image.width / this.#frames.max,
+      this.#image.height
+    );
+  }
+
+  getPosition() {
+    return this.#position;
   }
 
   moveUp(value) {
-    this.#position.y+=value;
+    this.#position.y += value;
   }
   moveDown(value) {
-    this.#position.y-=value;
+    this.#position.y -= value;
   }
   moveLeft(value) {
-    this.#position.x+=value;
+    this.#position.x += value;
   }
   moveRight(value) {
-    this.#position.x-=value;
+    this.#position.x -= value;
   }
 }
